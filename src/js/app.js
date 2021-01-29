680b607c28965d18133b0495f413a96129e175b2
Evgeny <salabaikin@mail.ru>
2021-01-29 18:38:21 +0300

    Add first page

--- src/js/app.js
+++ src/js/app.js
@@ -39,5 +39,32 @@ document.addEventListener("DOMContentLoaded", function() {
   const tabs = new Tabs(tabLinks, tabContent);
   tabs.render();
 
+  // Popular slider
+  const popularSwiper = new Swiper('.popular-slider.swiper-container', {
+    loop: false,
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.popular-slider-next',
+      prevEl: '.popular-slider-prev',
+    },
+  });
+
+  // Crop-Text
+  function cropText(selector, size) {
+    const cropElement = document.querySelectorAll(selector), // выбор элементов 
+    endCharacter = '...'; // окончание 
+    cropElement.forEach(function(el) {
+      let text = el.innerText;
+
+      if (el.innerText.length > size) {
+          text = text.substr(0, size);
+          el.innerText = text + endCharacter;
+      }
+    });
+  }
+  cropText('.popular-item__title a', 41);
+  cropText('.popular-item__category-name a', 30);
+
 });
 
